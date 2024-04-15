669842f14f13116b667523673633a7b48372fe68
Build Pipeline <none@email.com>
2024-04-15 19:02:27 +0200

    Add project files and configurations for tales-dotnet-console-app

---
+++ Data.cs
@@ -0,0 +1,88 @@
+// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+public enum DocumentType
+{
+    P,
+    ID,
+    DL
+}
+
+
+public class Data
+{
+    public static string[] acceptedCountries = { "ESP", "FRA", "POR", "AND", "MOR" };
+    public int scanId { get; set; }
+    public DocumentType DocumentType { get; set; }
+    public string IssuingCountry { get; set; }
+    public string LastName { get; set; }
+
+    public string FirstName { get; set; }
+    public string DocumentNumber { get; set; }
+    public string Nationality { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public DateTime DateOfExpiry { get; set; }
+
+    public Data(string[] dataElementFragments)
+    {
+        MapDocumentType(dataElementFragments);
+        scanId = int.Parse(dataElementFragments[0]);
+        IssuingCountry = dataElementFragments[2][..3];
+        LastName = dataElementFragments[3];
+        FirstName = dataElementFragments[4];
+        DocumentNumber = dataElementFragments[5];
+        Nationality = dataElementFragments[6][..3];
+        DateOfBirth = DateTime.ParseExact(dataElementFragments[7], "yyMMdd", CultureInfo.InvariantCulture);
+        DateOfExpiry = DateTime.ParseExact(dataElementFragments[8], "yyMMdd", CultureInfo.InvariantCulture);
+    }
+
+    private void MapDocumentType(string[] dataElementFragments)
+    {
+        switch (dataElementFragments[1])
+        {
+            case "P":
+                DocumentType = DocumentType.P;
+                break;
+            case "ID":
+                DocumentType = DocumentType.ID;
+                break;
+            case "DL":
+                DocumentType = DocumentType.DL;
+                break;
+            default:
+                throw new ArgumentException("Invalid document type");
+        }
+    }
+    public static bool IsValidData(Data data)
+    {
+        return data.DateOfExpiry > DateTime.Now &&
+                acceptedCountries.Contains(data.IssuingCountry) &&
+                acceptedCountries.Contains(data.Nationality);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        Data data = (Data)obj;
+        return (
+            DocumentNumber == data.DocumentNumber &&
+            IssuingCountry == data.IssuingCountry &&
+            DocumentType == data.DocumentType
+        ) || (
+            FirstName == data.FirstName &&
+            LastName == data.LastName &&
+            Nationality == data.Nationality &&
+            DateOfBirth == data.DateOfBirth
+        );
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(FirstName, LastName, Nationality, DateOfBirth, DocumentNumber, IssuingCountry, DocumentType);
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+
+Console.WriteLine("*** Welcome to Tales C# Test ***");
+
+
+Console.WriteLine("Please enter data:");
+
+List<string> data = ReadData();
+
+List<int> dataToRemove = DataToRemove(data);
+
+Console.WriteLine("Thank you for entering data");
+string dataToRemoveString = string.Join(",", dataToRemove);
+Console.WriteLine($"The following elements will need to be removed: {dataToRemoveString}");
+
+static List<string> ReadData()
+{
+    List<string> data = new List<string>();
+    string? currentLine;
+    do
+    {
+        currentLine = Console.ReadLine();
+        if (!string.IsNullOrEmpty(currentLine))
+        {
+            data.Add(currentLine);
+        }
+    } while (string.IsNullOrEmpty(currentLine));
+    return data;
+}
+
+static List<int> DataToRemove(List<string> data)
+{
+    bool parseSuccess = int.TryParse(data[0], out int dataLength);
+
+    if (!parseSuccess)
+    {
+        Console.WriteLine("Invalid data length");
+        return null;
+    }
+
+
+    List<Data> filteredData = new List<Data>();
+    List<int> elementsToRemove = [];
+
+    for (int i = 1; i < dataLength; i++)
+    {
+        string[] dataElementFragments = data[i].Split(",");
+        Data dataItem = new(dataElementFragments);
+        filteredData.Add(dataItem);
+        if (!Data.IsValidData(dataItem) && filteredData.FirstOrDefault(filteredDataItem => filteredDataItem == filteredData[i]) != null)
+        {
+            elementsToRemove.Add(i);
+        }
+    }
+
+    return elementsToRemove;
+}
